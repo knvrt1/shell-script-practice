cf2f3045411f9978f97f0d29c2202168325c5ae3
unknown <knvrt1@gmail.com>
2025-02-24 14:24:07 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -3,7 +3,8 @@
 rpm -qa|grep -i mysql &> /dev/null
 if [ $? -ne 0 ]
 then 
-echo "Installing mysql:" yum install -y mysql
+echo "Installing mysql:" 
+yum install -y mysql
 else
 echo "Mysql is already installed"
 fi
