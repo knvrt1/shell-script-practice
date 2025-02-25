e22259cd272dd20ea04e7f13f1e96d42da26b36c
unknown <knvrt1@gmail.com>
2025-02-25 13:16:01 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -4,7 +4,9 @@
 rpm -qa|grep -i nginx
 if [ $? -ne 0 ]
 then
-echo "Installing nginx:" yum install -y nginx &> /tmp/nginx.out
+echo "Installing nginx:" 
+yum install -y nginx &> /tmp/nginx.out
 else
 echo "Nginx is already installed"
 fi
+
