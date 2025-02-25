bb1100fc039561701e14842a7831c8f3084e5944
unknown <knvrt1@gmail.com>
2025-02-25 13:11:28 +0530

    functions

---
+++ functions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## checking nginx installation ##
+rpm -qa|grep -i nginx
+if [ $? -ne 0 ]
+then
+echo "Installing nginx:" yum install -y nginx &> /tmp/nginx.out
+else
+echo "Nginx is already installed"
+fi
